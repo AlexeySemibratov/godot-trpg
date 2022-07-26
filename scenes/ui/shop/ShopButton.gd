7df74712883b884c3486e7b78f709b2c705d9660
Alexey Semibratov <jaxel1999@gmail.com>
2022-07-26 23:16:25 +0700

    new shop card item layout, new shop panel

---
+++ scenes/ui/shop/ShopButton.gd
@@ -0,0 +1,11 @@
+extends MarginContainer
+
+signal on_card_pressed
+
+onready var button = $MarginContainer/VBoxContainer/BuildTowerButton
+onready var tower_texture_button = $MarginContainer/VBoxContainer/BuildTowerButton/BuildButton
+
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+		   emit_signal("on_card_pressed")
--- scenes/ui/shop/ShopItems.gd
+++ scenes/ui/shop/ShopItems.gd
@@ -1,27 +1,25 @@
-extends Control
+extends PanelContainer
 
 signal on_button_clicked(tower)
 
-onready var towers_container = $ScrollContainer/ButtonsContainer
+onready var towers_container = $MarginContainer/VBoxContainer/ScrollContainer/ButtonsContainer
 
 var button_scene = load("res://scenes/ui/shop/ShopButton.tscn")
 
 func add_towers(towers: Dictionary):
 	for tower in towers.values():
-		var button = _create_button(tower)
+		var button = button_scene.instance()
 		towers_container.add_child(button)
-
-func _create_button(tower):
-	var button = button_scene.instance()
-	button.get_node("BuildTowerButton/BuildButton").connect("pressed", self, "_on_button_clicked", [tower])
-	
+		button.connect("on_card_pressed", self, "_on_button_clicked", [tower])
+		_set_tower_texture(button.tower_texture_button, tower)
+		
+func _set_tower_texture(button: TextureButton, tower):
 	var texture = ImageTexture.new()
 	var image = Image.new()
 	image.load(tower["icon"])
 	texture.create_from_image(image)
 	var icon = load(tower["icon"])
-	button.get_node("BuildTowerButton/BuildButton").set_normal_texture(texture)
-	return button
+	button.set_normal_texture(texture)
 	
 func _on_button_clicked(tower):
 	emit_signal("on_button_clicked", tower)
